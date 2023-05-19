bbee5b0fafaa9f1d81db4e2e56ef264a72fdc324
MaristelaP <marispeg@gmail.com>
2023-05-19 15:06:11 -0300

    criação do post em andamento

--- src/main/java/br/com/lagoinha/controledespesas/controller/DespesasController.java
+++ src/main/java/br/com/lagoinha/controledespesas/controller/DespesasController.java
@@ -1,5 +1,27 @@
 package br.com.lagoinha.controledespesas.controller;
 
+import br.com.lagoinha.controledespesas.entity.Despesas;
+import br.com.lagoinha.controledespesas.service.DespesasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("despesas")
 public class DespesasController {
+
+    @Autowired
+    private DespesasService despesasService;
+
+    @PostMapping
+    public Despesas post(@RequestBody Despesas despesas) throws Exception {
+
+
+        return despesasService.salvarDespesas(despesas);
+    }
+
+
 
 }
--- src/main/java/br/com/lagoinha/controledespesas/service/DespesasService.java
+++ src/main/java/br/com/lagoinha/controledespesas/service/DespesasService.java
@@ -1,5 +1,6 @@
 package br.com.lagoinha.controledespesas.service;
 
+import br.com.lagoinha.controledespesas.entity.Despesas;
 import br.com.lagoinha.controledespesas.repository.DespesasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,16 @@ public class DespesasService {
 
     @Autowired
     private DespesasRepository despesasRepository;
+
+    public Despesas salvarDespesas(Despesas despesas) throws Exception {
+        despesas.setPendente(true);
+
+        if (despesas.getCredor() == null || despesas.getCredor().isEmpty()){
+            throw new Exception("Por favor, informe o Credor.");
+        }
+        if (despesas.getValorDespesa() == null) {
+            throw new Exception("Por favor, informe o valor.");
+        }
+        return despesasRepository.save(despesas);
+    }
 }
